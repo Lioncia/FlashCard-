63c787a71d9ae4342a2206ae6838904daf8b6399
Lioncia <00001107361631sp@al.educacao.sp.gov.br>
2024-11-29 13:47:40 -0300

    Perguntas.js

---
+++ Perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Escócia?',
+    'A capital da Escócia é Edimburgo'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é uma célula?',
+    'É a unidade estrutural e funcional dos seres vivos'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "obrigado" em Inglês?',
+    'Obrigado em ingles é thank you'
+)
